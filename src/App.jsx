6e7ec1d244bea55bd1bc30df4d0a930d2de96316
Bryan Cusme <bryan.cusme@devoteam.com>
2023-03-05 11:38:04 +0100

    add: mvp solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import withResults from './mocks/with-results.json'
 
 function App () {
-  const [count, setCount] = useState(0)
-
+  function handleSubmit (e) {
+    e.preventDetault()
+  }
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='app'>
+      <header>
+        <h1>Moovie searcher</h1>
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder='Batman, Star wars...' />
+          <button type='submit'>search</button>
+        </form>
+      </header>
+      <main className='moovies-container'>
+        {withResults.Response === 'True'
+          ? (withResults.Search.map(moovie => {
+              const { Title, Poster, Year, imdbID } = moovie
+              return (
+                <div key={imdbID} className='moovie-card'>
+                  <img src={Poster} alt={`Poster from the film ${Title}`} />
+                  <h4>{Title}</h4>
+                  <p>{Year}</p>
+                </div>
+              )
+            }))
+          : <p>No results</p>}
+      </main>
     </div>
   )
 }
